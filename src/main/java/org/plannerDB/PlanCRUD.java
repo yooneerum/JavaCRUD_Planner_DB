88b54cab5c4c36d6391fd196d0d4452b5a2d358f
yooneerum <yooneerum@gmail.com>
2025-11-17 21:46:23 +0900

    feat: add PlanCRUD.java class

---
+++ src/main/java/org/plannerDB/PlanCRUD.java
@@ -0,0 +1,22 @@
+package org.plannerDB;
+
+public class PlanCRUD implements ICRUD{
+    @Override
+    public Object add() {
+    }
+
+    @Override
+    public int updatePlan(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int deletePlan(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    }
+}
