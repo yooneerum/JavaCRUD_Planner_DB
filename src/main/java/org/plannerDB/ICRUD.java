fec50d65f5e58bf21830d2da926978ae85abb1f2
yooneerum <yooneerum@gmail.com>
2025-11-20 02:42:02 +0900

    feat: update data in DB

--- src/main/java/org/plannerDB/ICRUD.java
+++ src/main/java/org/plannerDB/ICRUD.java
@@ -3,8 +3,8 @@ package org.plannerDB;
 import java.sql.SQLException;
 
 public interface ICRUD {
-    public int add(Plan one) throws SQLException;
-    public void updatePlan();
+    int add(Plan one) throws SQLException;
+    int update(Plan one) throws SQLException;
     public void deletePlan();
     public void selectOne(int id);
 }
