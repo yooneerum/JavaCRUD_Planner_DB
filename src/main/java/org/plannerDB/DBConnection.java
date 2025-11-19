d5e7caffcc6b0fa1ddc04c8e4c346132095381a9
yooneerum <yooneerum@gmail.com>
2025-11-19 18:48:47 +0900

    feat: add DB connection, load data from DB

---
+++ src/main/java/org/plannerDB/DBConnection.java
@@ -0,0 +1,30 @@
+package org.plannerDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if(conn == null) {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                conn = DriverManager.getConnection("jdbc:sqlite:myplanner.db");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+    public static void closeConnection() {
+        if(conn != null) {
+            try {
+                conn.close();
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
